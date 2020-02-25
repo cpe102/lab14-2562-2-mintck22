a50c6be269dc136c5e3b66bc5b22b7f6bc7bc69e
Mint <chawanluck_k@hotmail.com>
2020-02-25 17:28:59 +0700

    1

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -1,11 +1,73 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
 
 int main(){
 	//Write your code here.
-	
-	
+	int a = 5;
+	char b = 'A';
+	char &c = b;
+	int *x = &a;
+	char *y = &b;
+	int **z = &x;
+
+	cout << "a = " << a << "\n";
+	cout << "b = " << b << "\n";
+	cout << "c = " << c << "\n";
+	cout << "x = " << x << "\n";
+	cout << "y = " << (int*)y << "\n";
+	cout << "z = " << z << "\n";
+
+	cout << "\n";
+
+	cout << "&a = " << &a << "\n";
+	cout << "&b = " << (int*)&b << "\n";
+	cout << "&c = " << (int*)&c << "\n";
+	cout << "&x = " << &x << "\n";
+	cout << "&y = " << &y << "\n";
+	cout << "&z = " << &z << "\n";
+
+	c = 'F';
+	cout << "\n";
+
+	cout << "a = " << a << "\n";
+	cout << "b = " << b << "\n";
+	cout << "c = " << c << "\n";
+	cout << "x = " << x << "\n";
+	cout << "y = " << (int*)y << "\n";
+	cout << "z = " << z << "\n";
+
+	*y = 'W';
+	cout << "\n";
+
+	cout << "a = " << a << "\n";
+	cout << "b = " << b << "\n";
+	cout << "c = " << c << "\n";
+	cout << "x = " << x << "\n";
+	cout << "y = " << (int*)y << "\n";
+	cout << "z = " << z << "\n";
+
+	*x = 6;
+	cout << "\n";
+
+	cout << "a = " << a << "\n";
+	cout << "b = " << b << "\n";
+	cout << "c = " << c << "\n";
+	cout << "x = " << x << "\n";
+	cout << "y = " << (int*)y << "\n";
+	cout << "z = " << z << "\n";
+
+	**z = 7;
+	cout << "\n";
+
+	cout << "a = " << a << "\n";
+	cout << "b = " << b << "\n";
+	cout << "c = " << c << "\n";
+	cout << "x = " << x << "\n";
+	cout << "y = " << (int*)y << "\n";
+	cout << "z = " << z << "\n";
+
+
 	
 	return 0;
 }
